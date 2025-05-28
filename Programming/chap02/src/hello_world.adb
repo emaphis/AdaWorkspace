2e053fc6517dfe50a08f767f125b958892a95bf2
emaphis <emaphis85@gmail.com>
2025-05-27 23:38:57 -0400

    Update hello_world to macth the book.

--- Programming/chap02/src/hello_world.adb
+++ Programming/chap02/src/hello_world.adb
@@ -1,7 +1,12 @@
 with Ada.Text_IO;
 
 procedure Hello_world is
+
+   -- Program from page 50.
+
+   Greeting : constant String := "Hello Ada World"; -- The message
    
 begin
-   Ada.Text_IO.Put_Line ("Hello Ada World!");
+   Ada.Text_IO.Put (Item => Greeting);
+   Ada.Text_IO.New_Line;
 end Hello_world;
