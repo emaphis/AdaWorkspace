b8f0048ee810dad387a2444018754319ac5e3466
emaphis <emaphis85@gmail.com>
2025-05-27 23:20:46 -0400

    Create a Chapter 2 project.

---
+++ Programming/Readme.md
@@ -0,0 +1,5 @@
+# Programming and Problem Solving with Ada 95
+
+By Nell Dale, Chip Weems, John McCormic
+
+## Chapter summary
---
+++ Programming/chap02/src/hello_world.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+
+procedure Hello_world is
+   
+begin
+   Ada.Text_IO.Put_Line ("Hello Ada World!");
+end Hello_world;
